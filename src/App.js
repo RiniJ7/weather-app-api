c6379b8e54f4fb306579a67e5a78959802388923
rinij <rinimani7@gmail.com>
2023-12-29 17:12:12 -0700

    added open weather app api key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+
+const url = `api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=fe9d5f6f0f3c4b2ac5d8c9f3bd88a60f`;
+
+
   return (
     <div className="App">
  
